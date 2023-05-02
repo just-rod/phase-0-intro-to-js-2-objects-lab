28d30f1a385e72d3fd887f9e7b9070a104ee3d6b
Justin Rodriguez <justin.r@me.com>
2023-05-02 15:18:07 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+const employee = {
+    name: "Justin Rodriguez",
+    address: "298 Convent Ave Apt 2B New York, NY 10031"
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    return {...obj, [key]: value,};
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key] = value;
+
+    return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key){
+    const newObj = {...obj};
+    delete newObj[key];
+
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    delete obj[key];
+    return obj;
+}
